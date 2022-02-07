a8bea4a8a2f797e13617f014ed86e427fc52d114
Luis Fernando <lfmnovaes@gmail.com>
2022-02-07 13:01:44 -0300

    Create group and investment models

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  resources :investments
+  resources :groups
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
