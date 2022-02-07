8417eb4df73b9b1d1aad0584e237fbe5dfc59e33
Luis Fernando <lfmnovaes@gmail.com>
2022-02-07 13:11:30 -0300

    Install devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,7 @@
 Rails.application.routes.draw do
+  devise_for :users
   resources :investments
   resources :groups
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  root to: 'groups#index'
 end
